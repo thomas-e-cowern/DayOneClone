e802536bb6890aeb4ec4d018e6ebb0ded8163262
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-07-25 08:27:40 -0400

    my journal initial commti

---
+++ MyJournal/MyJournal/MyJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJournalApp.swift
+//  MyJournal
+//
+//  Created by Thomas Cowern on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct MyJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
